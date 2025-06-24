d9df1a8634c0420a49751309ba52420c39802e71
akshay c r <117341539+akshaycr78@users.noreply.github.com>
2025-06-24 12:48:21 +0530

    Update post.routes.ts

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -12,22 +12,11 @@ import upload from "../utlis/multerConfig";
 
 const router = express.Router();
 
-// Create a post
 router.post("/add", authenticateJWT, upload.single("image"), createPost);
-
-// Edit a post
 router.put("/edit/:id", authenticateJWT, updatePost);
-
-// Delete a post
 router.delete("/delete/:id", authenticateJWT, deletePost);
-
-// Like or Unlike a post
 router.put("/like/:id", authenticateJWT, likePost);
-
-// Share a post
 router.put("/share/:id", authenticateJWT, sharePost);
-
-// Get list of users who liked a post
 router.get("/liked-users/:id", authenticateJWT, getLikedUsers);
 
 export default router;
